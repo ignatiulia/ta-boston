fcf4df55a964e98d4eb0d154c51d028451826c51
ignatiulia <ignatiulia71@gmail.com>
2022-11-03 19:58:16 +0200

    TA start 3.11

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>Hello, Thread Affair!
       <i className="fa-solid fa-house"></i>
+      <img src="/images/logo.jpg" alt="" />
       </div>
   )
 }
